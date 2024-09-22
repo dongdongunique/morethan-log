9b75c11014e826ff668308f2212cf76bb8fe9a71
Yunhao Chen <89110975+dongdongunique@users.noreply.github.com>
2024-09-22 21:26:42 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `SIDE_attack`,
-      #href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
